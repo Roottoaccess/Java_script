4baab81ffc818e77673cd7215da33663b60c6edb
NetworkCrab <network123crab@gmail.com>
2024-01-31 22:35:59 +0530

    This is the conditional statement in javascript

---
+++ conditional_statement.js
@@ -0,0 +1,34 @@
+// if , else if and else statement important concept
+console.log("hello");
+var x = 10;
+if(x > 2 && x < 50){
+    console.log("hi");
+    console.log("ok");
+}
+
+console.log("buy");
+
+// even odd number
+if(x % 2 == 0)
+{
+    console.log("even number");
+} else if(x % 2 != 0){
+    console.log("odd number");
+}
+else
+{
+    console.log("give a number");
+}
+console.log("Iron man");
+
+let a = 88;
+let b = 82;
+let c = 84;
+
+if(a > b && a > c){
+    console.log(a,"is greater");
+} else if(b > a && b > c){
+    console.log(b,"is greater");
+} else {
+    console.log(c,"is greater");
+}
