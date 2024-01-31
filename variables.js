88ec101439465b3235543e65ed0e2bbb59c4a64a
NetworkCrab <network123crab@gmail.com>
2024-01-31 20:34:26 +0530

    variable concept in javascript

---
+++ variables.js
@@ -0,0 +1,40 @@
+// Variables in javascript
+// Variable is just a data which contain the data
+
+// var , let , const 
+// let num1 = 22;
+// let num2 = 23;
+
+
+// let result = num1 + num2;
+// console.log("result = ",result);
+// we can declare many variable through var
+// var x;
+// x = 10;
+// console.log("x = ",x);
+// x = 20;
+// console.log(x);
+
+// x = "Biswarup Dutta";
+// console.log("The name is:",x);
+// console.log("The type of x = ",typeof(x));
+
+// let x = 18;
+// console.log(x);
+
+// let x = 15;
+// console.log(x);
+
+// const y = 10;
+// console.log(y);
+
+
+// console.log(x);
+// if we create a let variable then it will only perfome inside the block
+// but in the case of var we can access the element outside the block also
+{
+    var x = 10;
+    x = 20;
+    console.log(x);
+}
+console.log(x);
