67cf809ef9b419e112b03f92e3eb0e6f538bb2fa
NetworkCrab <network123crab@gmail.com>
2024-02-01 12:53:26 +0530

    This is the arrow function which makes function more valuable and looks great after applying it

---
+++ arrow_function.js
@@ -0,0 +1,13 @@
+// This is the arrow function concept in javascript
+const demo = () =>
+{
+    console.log("hello");
+}
+
+demo();
+
+const sum = (x,y) => {
+    return x+y;
+}
+let z = sum(30,50);
+console.log("The result is: ",z);
