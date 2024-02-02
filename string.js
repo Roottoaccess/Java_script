92e8ec3485ef928b6268ce30a28e6828527554cc
NetworkCrab <network123crab@gmail.com>
2024-02-02 11:18:14 +0530

    This are the string and number methods important

---
+++ string.js
@@ -0,0 +1,31 @@
+var text = "Welcome to sbs online classes \n name is biswarup dutta";
+console.log(text);
+var demo = "This is the printing of \\n";
+console.log(demo);
+// To find the length of all the numbers
+var n = text.length;
+console.log(n);
+// This is the method to bring all the numbers in lowercase
+var Lower_case = text.toLowerCase();
+console.log(Lower_case);
+
+// Uppercase
+console.log(text.toUpperCase());
+
+// slice
+console.log(text.slice(-4));
+
+// white space remove
+var x = "    white space    ";
+console.log(x.trim()); // trimStart trimEnd
+
+// Getting 2 digit after decimal
+var num = 9.4579
+console.log(num.toFixed(2));
+
+// convert the number into string
+var num = 958;
+var str = num.toString();
+console.log(str,typeof str);
+
+
