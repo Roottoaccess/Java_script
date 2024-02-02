785ba97a22f0d3efdca22e5167ed8ca17fe79963
NetworkCrab <network123crab@gmail.com>
2024-02-02 11:27:12 +0530

    This is the concept of switch_case in javascript

---
+++ switch_case.js
@@ -0,0 +1,17 @@
+// This is the switch case
+// Structure of switch case
+let x = 5;
+switch(x)
+{
+    case 1:
+        console.log("hello");
+        break;
+    case 2:
+        console.log("hi");
+        break;
+    case 3:
+        console.log("bye");
+        break;
+    default:
+        console.log("This is the default path");
+}
