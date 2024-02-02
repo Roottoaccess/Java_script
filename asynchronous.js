a5be1c840d66e6158c09ead0b813ef10bede25bb
NetworkCrab <network123crab@gmail.com>
2024-02-02 11:57:24 +0530

    This is the asynchronous program

---
+++ asynchronous.js
@@ -0,0 +1,11 @@
+// Advance javascript concept
+// If we want to place a delay in the code
+console.log("hi");
+setTimeout(function()
+{
+    console.log("hello");
+},2000);
+for(var i = 0; i <= 5; i++)
+{
+    console.log(i);
+}
